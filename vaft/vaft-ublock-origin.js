49e7ceeee67087d0ee472fdf1f8ae743cf536bb4
ghostkill3r <50037383+ghostkill3r@users.noreply.github.com>
2022-09-05 09:13:42 +0200

    Update vaft-ublock-origin.js

--- vaft/vaft-ublock-origin.js
+++ vaft/vaft-ublock-origin.js
@@ -72,8 +72,8 @@ twitch-videoad.js application/javascript
         scope.MainUrlByUrl = [];
         scope.EncodingCacheTimeout = 60000;
         scope.DefaultProxyType = null;
-        scope.DefaultForcedQuality = 'source';
-        scope.DefaultProxyQuality = 'source';
+        scope.DefaultForcedQuality = '720p';
+        scope.DefaultProxyQuality = '720p';
     }
     declareOptions(window);
     var TwitchAdblockSettings = {
